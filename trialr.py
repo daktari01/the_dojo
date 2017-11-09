7d35a2c6270ed605b0fd4de4f41730cf375c29ae
Daktari <dindijjames@gmail.com>
2017-11-09 12:15:37 +0000

    [Trick] Got to write dictionaries to file and read from them

--- trialr.py
+++ trialr.py
@@ -1,16 +1,61 @@
-with open('members.txt') as file:
-    mdict={}
-    for line in file:
-        a, b, c, d = line.strip().split(':')
-        mdict[a] = b + ':' + c + ':' + d
+dict_A = {
+    'red':['Morris', 'Roy'],
+    'blue':['Oliver', 'Otos', 'Justo'],
+    'cyan':['Kipsangui'],
+    'magenta':['Eunice', 'Kenga', 'Galo', 'Orkoiyot']
+}
+'''
+dict_from_file = {}
+dict_A = {
+    'red':['Morris', 'Roy'],
+    'blue':['Oliver', 'Otos', 'Justo'],
+    'cyan':['Kipsangui'],
+    'magenta':['Eunice', 'Kenga', 'Galo', 'Orkoiyot']
+}
 
-a = input('ID: ')
-if a not in mdict:
-    print('ID {} not found'.format(a))
-else:
-    b, c, d = mdict[a].split(':')
-    d = input('phone: ')
-    mdict[a] = b + ':' + c + ':' + d  # update entry
-    with open('members.txt', 'w') as file:  # rewrite file
-        for id, values in mdict.items():
-            file.write(':'.join([id] + values.split(':')) + '\n')
+fout = "members.txt"
+fout_r = "members_r.txt"
+fo = open(fout, "w")
+fo_r = open(fout_r, "w")
+
+for k, v in dict_A.items():
+    fo.write(str(k) + '\n' + ' ---------------------------------------- '+ '\n' + str(v) + '\n\n')
+    fo_r.write(str(k) + ' = ' + str(v) + '\n')
+fo_r.close()
+fo.close()
+
+
+with open('members_r.txt') as f:
+    for line in f:
+        k, v = line.strip().split(' = ')
+        dict_from_file[k.strip()] = v.strip()
+
+f.close()
+print(dict_from_file)
+'''
+def write_read_dict_text(dict_to_read, write_file, read_file):
+    dict_from_file = {}
+    fout = write_file
+    fout_r = read_file
+    fo = open(fout, "w")
+    fo_r = open(fout_r, "w")
+    
+    for k, v in dict_to_read.items():
+        fo.write(str(k) + '\n' + ' ---------------------------------------- '+ '\n' + str(v) + '\n\n')
+        fo_r.write(str(k) + ' = ' + str(v) + '\n')
+    fo_r.close()
+    fo.close()
+    
+    
+    with open('members_r.txt') as f:
+        for line in f:
+            k, v = line.strip().split(' = ')
+            dict_from_file[k.strip()] = v.strip()
+    
+    f.close()
+    return dict_from_file
+    
+dict_b = write_read_dict_text(dict_A, 'trias.txt', 'trias_r.txt')
+print(dict_b)
+    
+
